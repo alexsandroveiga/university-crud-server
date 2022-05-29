24eaf595eeeca2e5e3d82e2b41f9b7ec9e856685
Alexsandro <sandro.alex_@hotmail.com>
2022-05-29 15:23:51 -0300

    feat: add search query

--- controllers/list_customers.py
+++ controllers/list_customers.py
@@ -7,8 +7,9 @@ class ListCustomersController(Controller):
   def perform(self, request):
     page = request.args.get("page", default=1, type=int)
     per_page = request.args.get("per_page", default=10, type=int)
+    search = request.args.get("search", default='', type=str)
     offset = (page - 1) * per_page
     limit = per_page
-    total = Customer.select().count()
-    customers = Customer.select().order_by(Customer.created_at.desc()).limit(limit).offset(offset)
+    total = Customer.select().where(Customer.name.contains(search) | Customer.email.contains(search)).count()
+    customers = Customer.select().where(Customer.name.contains(search) | Customer.email.contains(search)).order_by(Customer.created_at.desc()).limit(limit).offset(offset)
     return ok(list(customers.dicts()), headers={"X-Total-Count": total, 'X-Total-Pages': math.ceil(total / per_page)})
--- controllers/list_motorcycles.py
+++ controllers/list_motorcycles.py
@@ -8,8 +8,9 @@ class ListMotorcyclesController(Controller):
   def perform(self, request: Request):
     page = request.args.get("page", default=1, type=int)
     per_page = request.args.get("per_page", default=10, type=int)
+    search = request.args.get("search", default='', type=str)
     offset = (page - 1) * per_page
     limit = per_page
-    total = Motorcycle.select().count()
-    motos = Motorcycle.select().order_by(Motorcycle.created_at.desc()).limit(limit).offset(offset)
+    total = Motorcycle.select().where(Motorcycle.brand.contains(search) | Motorcycle.model.contains(search)).count()
+    motos = Motorcycle.select().where(Motorcycle.brand.contains(search) | Motorcycle.model.contains(search)).order_by(Motorcycle.created_at.desc()).limit(limit).offset(offset)
     return ok(list(motos.dicts()), headers={"X-Total-Count": total, 'X-Total-Pages': math.ceil(total / per_page)})
