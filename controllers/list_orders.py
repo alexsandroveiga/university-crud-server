8f5845c7882c813321f0029734064f71b85c76fa
Alexsandro <sandro.alex_@hotmail.com>
2022-06-15 23:47:38 -0300

    fix: list order use case

--- controllers/list_orders.py
+++ controllers/list_orders.py
@@ -8,5 +8,8 @@ class ListOrdersController(Controller):
     order = Order.select().join(Customer).dicts().order_by(Order.id.desc())
     for item in order:
       item['customer'] = Customer.select().where(Customer.id == item['customer']).first().__data__
-      item['motorcycles'] = list(OrderMotorcycles.select().where(OrderMotorcycles.order == item['id']).dicts())
+      order_motorcycle = OrderMotorcycles.select().where(OrderMotorcycles.order == item['id']).dicts()
+      for item_motorcycle in order_motorcycle:
+        item_motorcycle['motorcycle'] = Motorcycle.select().where(Motorcycle.id == item_motorcycle['motorcycle']).first().__data__
+      item['motorcycles'] = list(order_motorcycle)
     return ok(list(order))
