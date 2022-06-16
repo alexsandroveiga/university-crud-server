d06136b1c8ca84a1f6a3901f4b2a6fb48cf62218
Alexsandro <sandro.alex_@hotmail.com>
2022-06-15 21:58:00 -0300

    feat: update order use cases

--- controllers/create_order.py
+++ controllers/create_order.py
@@ -4,7 +4,16 @@ from helpers import *
 
 class CreateOrderController(Controller):
   def perform(self, request):
-    order = Order.create(**request.json)
-    for item in request.json['motorcycles']:
-      OrderMotorcycles.create(order=order, motorcycle=item['motorcycle'], quantity=item['quantity'], price=item['price'])
-    return ok(order.__data__)
+    try:
+      order = Order.create(**request.json)
+      for item in request.json['motorcycles']:
+        moto = Motorcycle.select().where(Motorcycle.id == item['motorcycle']).first()
+        if moto:
+          quantity = moto.quantity - item['quantity']
+          if quantity < 0:
+            raise(Exception('Not enough motorcycles'))
+          Motorcycle.update({ Motorcycle.quantity: quantity }).where(Motorcycle.id == item['motorcycle']).execute()
+        OrderMotorcycles.create(order=order, motorcycle=item['motorcycle'], quantity=item['quantity'], price=item['price'])
+      return ok(order.__data__)
+    except Exception as error:
+      return bad_request(str(error))
--- controllers/list_orders.py
+++ controllers/list_orders.py
@@ -1,12 +1,11 @@
 import math
 from controllers import Controller
 from infra import *
-from helpers import ok
+from helpers import *
 
 class ListOrdersController(Controller):
   def perform(self, request):
-    id = request.args.get("id", default=1, type=str)
-    order = Order.select().where(Order.id == id).dicts()
+    order = Order.select().join(Customer).dicts().order_by(Order.id.desc())
     for item in order:
       item['customer'] = Customer.select().where(Customer.id == item['customer']).first().__data__
       item['motorcycles'] = list(OrderMotorcycles.select().where(OrderMotorcycles.order == item['id']).dicts())
